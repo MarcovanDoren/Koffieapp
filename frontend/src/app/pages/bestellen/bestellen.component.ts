6d33941393110208abd64cbd7c5d4b2c67dd307e
jorisvdinther <j.vandinther@kw1c.nl>
2018-09-20 20:10:15 +0200

    Order bestelling to verify

--- frontend/src/app/pages/bestellen/bestellen.component.ts
+++ frontend/src/app/pages/bestellen/bestellen.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Product} from '../../@core/product';
+import { Product } from '../../@core/product';
+import { AvailableCoffeeService } from '../../@core/available-coffee.service';
 
 @Component({
   selector: 'app-bestellen',
@@ -7,21 +8,30 @@ import {Product} from '../../@core/product';
   styleUrls: ['./bestellen.component.scss']
 })
 export class BestellenComponent implements OnInit {
-  bestellingen = [ ];
-
-  constructor() { }
+  bestellingen = [];
+  availableCoffees;
+  constructor(private availableCoffeeService: AvailableCoffeeService) { }
 
   ngOnInit() {
+    this.availableCoffees = this.availableCoffeeService.getCoffee();
   }
 
   addToOrder(product: string, aantal: number): void {
     for (const s of this.bestellingen) {
-        if (s.name === product) {
-         s.aantal++;
-          return;
-        }
+      if (s.name === product) {
+        s.aantal++;
+        return;
+      }
     }
     const newProduct = new Product(product ? product : 'Koffie', aantal ? aantal : 1);
     this.bestellingen.push(newProduct);
+  }
+
+  deleteFromOrder(bestelling: Product){
+    this.bestellingen.splice(this.bestellingen.indexOf(bestelling),1);
+  }
+
+  clearCart(): void {
+    this.bestellingen = [];
   }
 }
